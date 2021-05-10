3d8e6b87c8962d6b0877e6cf6a05dfda8eb47a07
shaperbr <arthur@afabrica.net>
2021-05-10 16:28:42 -0300

    Add files via upload

--- util/util.go
+++ util/util.go
@@ -7,7 +7,9 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/common/math"
+
 )
 
 var Ether = math.BigPow(10, 18)
@@ -35,7 +37,7 @@ func MakeTimestamp() int64 {
 func GetTargetHex(diff int64) string {
 	difficulty := big.NewInt(diff)
 	diff1 := new(big.Int).Div(pow256, difficulty)
-	return string(common.ToHex(diff1.Bytes()))
+        return string(hexutil.Encode(diff1.Bytes()))
 }
 
 func TargetHexToDiff(targetHex string) *big.Int {
